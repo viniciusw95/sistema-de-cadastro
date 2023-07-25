1a756bed0b2e7c0f8d799098b93a67e579a389b5
viniciusw95 <viniciusvw18@hotmail.com>
2023-07-25 17:49:33 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados MySQL");
-
+// mais um console de teste
 console.log("O devpartner1 editou esse documento sem você ver durante a aula!");
-
+console.log("Enviando para o GitHub");
