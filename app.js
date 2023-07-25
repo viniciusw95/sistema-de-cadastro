0d9ee66a799e61f064899c35e87475d5f73adc1a
Werneck Vinicius <131465905+devpartner1@users.noreply.github.com>
2023-07-25 17:31:57 -0300

    console indicando modificacoes do devpartner1

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("aplicação está se conectando ao banco de dados MySQL");
+
+console.log("O devpartner1 editou esse documento sem você ver durante a aula!");
+
